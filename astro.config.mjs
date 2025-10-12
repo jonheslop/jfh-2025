95e0d562c1901acd9579ba3442590e93f19cda9e
Jon Heslop <jon@jonheslop.com>
2025-10-12 22:06:26 +0100

    Add halbfett

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,12 +10,36 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
     plugins: [tailwindcss()],
   },
 
   adapter: cloudflare(),
 
+  experimental: {
+    fonts: [
+      {
+        provider: "local",
+        name: "soehne",
+        cssVariable: "--font-soehne",
+        variants: [
+          {
+            weight: 400,
+            style: "normal",
+            src: ["./src/fonts/soehne-web-buch.woff2"],
+          },
+          {
+            weight: 400,
+            style: "italic",
+            src: ["./src/fonts/soehne-web-buch-kursiv.woff2"],
+          },
+          {
+            weight: 600,
+            style: "normal",
+            src: ["./src/fonts/soehne-halbfett.woff2"],
+          },
+        ],
+      },
+    ],
   },
 
   integrations: [mdx()],
