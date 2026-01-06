c3e7fb969b3703a75042132359f5f2a10c380151
Jon Heslop <jon@jonheslop.com>
2026-01-06 19:30:58 +0000

    Turso client

---
+++ src/utils/turso.ts
@@ -0,0 +1,18 @@
+import { createClient } from "@libsql/client/web";
+import { type Row } from "@libsql/client";
+
+export const turso = (
+  config = {
+    url: import.meta.env.TURSO_DATABASE_URL ?? process.env.TURSO_DATABASE_URL,
+    authToken: import.meta.env.TURSO_AUTH_TOKEN ?? process.env.TURSO_AUTH_TOKEN,
+  },
+) => createClient(config);
+
+export type Link = Row & {
+  id: number;
+  date: string;
+  url: string;
+  tags: string;
+  type: string;
+  title: string;
+};
