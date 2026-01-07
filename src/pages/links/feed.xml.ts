f6d818ca0922fc922c7b4e4b95774d358355cdf2
Jon Heslop <jon@jonheslop.com>
2026-01-07 22:22:59 +0000

    Add RSS feed

---
+++ src/pages/links/feed.xml.ts
@@ -0,0 +1,23 @@
+import rss from "@astrojs/rss";
+import { turso, type Link } from "../../utils/turso";
+
+export async function GET(context: { site: string }) {
+  const { rows } = await turso().execute(
+    "SELECT * FROM links ORDER BY id DESC",
+  );
+  const links = rows as Link[];
+  return rss({
+    title: "Links - Jon Heslop",
+    description:
+      "Links to things I’ve read/watched/seen around the web, saved here for posterity.",
+    site: context.site,
+    items: links.map((link) => {
+      return {
+        title: link.title,
+        pubDate: new Date(link.date),
+        content: link.comment || "",
+        link: `/links/${link.id}/`,
+      };
+    }),
+  });
+}
